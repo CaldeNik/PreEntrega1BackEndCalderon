5ef5e50d3b3fbc3df335acff0c40d392474a7f11
CaldeNik <calderondpc@gmail.com>
2023-05-24 12:33:24 -0300

    Se agrega CartManager.class.js

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,114 +1,27 @@
-import express from 'express';
-import fs from 'fs';
-import { v4 as uuidv4 } from 'uuid';
+import { Router } from 'express';
+import ProductManager from '../classes/ProductsManager.class.js';
 
-const router = express.Router();
+const router = Router();
 
-router.get('/', (req, res) => {
-  const { limit } = req.query;
-  const products = JSON.parse(fs.readFileSync('productos.json', 'utf8'));
+const productManager = new ProductManager();
 
-  if (limit) {
-    const limitedProducts = products.slice(0, parseInt(limit));
-    res.json(limitedProducts);
-  } else {
-    res.json(products);
-  }
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const product = await productManager.findProductById(id);
+  res.send({ product });
 });
 
-router.get('/:pid', (req, res) => {
-  const { pid } = req.params;
-  const products = JSON.parse(fs.readFileSync('productos.json', 'utf8'));
-  const product = products.find((p) => p.id === parseInt(pid));
-
-  if (product) {
-    res.json(product);
-  } else {
-    res.status(404).json({ error: 'Producto no encontrado' });
-  }
+router.get("/", async (req, res) => {
+  const products = await productManager.findProduct();
+  res.send({ products });
 });
 
-router.post('/', (req, res) => {
-  const { title, description, code, price, status = true, stock, category, thumbnails } = req.body;
+router.post("/", async (req, res) => {
+  console.log(req.body);
+  const product = req.body;
 
-  if (!title || !description || !code || !price || !stock || !category) {
-    res.status(400).json({ error: 'Faltan campos obligatorios' });
-    return;
-  }
-
-  const products = JSON.parse(fs.readFileSync('productos.json', 'utf8'));
-  const id = uuidv4();
-  const newProduct = {
-    id,
-    title,
-    description,
-    code,
-    price,
-    status,
-    stock,
-    category,
-    thumbnails,
-  };
-
-  products.push(newProduct);
-
-  fs.writeFileSync('productos.json', JSON.stringify(products, null, 2));
-
-  res.json(newProduct);
-});
-
-router.put('/:pid', (req, res) => {
-  const { pid } = req.params;
-  const { title, description, code, price, status, stock, category, thumbnails } = req.body;
-
-  if (!title || !description || !code || !price || !stock || !category) {
-    res.status(400).json({ error: 'Faltan campos obligatorios' });
-    return;
-  }
-
-  const products = JSON.parse(fs.readFileSync('productos.json', 'utf8'));
-  const productIndex = products.findIndex((p) => p.id === parseInt(pid));
-
-  if (productIndex === -1) {
-    res.status(404).json({ error: 'Producto no encontrado' });
-    return;
-  }
-
-  const updatedProduct = {
-    id: pid,
-    title,
-    description,
-    code,
-    price,
-    status,
-    stock,
-    category,
-    thumbnails,
-  };
-
-  products[productIndex] = updatedProduct;
-
-  fs.writeFileSync('productos.json', JSON.stringify(products, null, 2));
-
-  res.json(updatedProduct);
-});
-
-router.delete('/:pid', (req, res) => {
-  const { pid } = req.params;
-
-  const products = JSON.parse(fs.readFileSync('productos.json', 'utf8'));
-  const productIndex = products.findIndex((p) => p.id === parseInt(pid));
-
-  if (productIndex === -1) {
-    res.status(404).json({ error: 'Producto no encontrado' });
-    return;
-  }
-
-  const deletedProduct = products.splice(productIndex, 1)[0];
-
-  fs.writeFileSync('productos.json', JSON.stringify(products, null, 2));
-
-  res.json(deletedProduct);
+  await productManager.createProduct(product);
+  res.send({ status: "success" });
 });
 
 export default router;
