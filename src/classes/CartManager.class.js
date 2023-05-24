b09dfe5252c7c3e80dec68ec030e0694254eac02
CaldeNik <calderondpc@gmail.com>
2023-05-24 13:12:26 -0300

    corrigiendo funcionamiento del carrito

--- src/classes/CartManager.class.js
+++ src/classes/CartManager.class.js
@@ -4,30 +4,55 @@ import { v4 as uuidV4 } from "uuid";
 const path = "src/classes/files/carts.json";
 
 export default class CartManager {
-    consultarCarts = async () => {
-      console.log("existe", fs.existsSync(path));
-      if (fs.existsSync(path)) {
-        const data = await fs.promises.readFile(path, "utf-8");
-        const carts = JSON.parse(data);
-        return carts;
-      } else {
-        return [];
-      }
-    };
-  
-    createCart = async () => {
-      const carts = await this.findCart();
-      carts.push({ id: uuidV4(), products: [] });
-      return await fs.promises.writeFile(path, JSON.stringify(carts, null, "\t"));
-    };
-  
-    findCartById = async (id) => {
-      const carts = await this.findCart();
-  
-      const cart = carts.find((cart) => {
-        return cart.id == id;
-      });
-  
-      return cart ? cart : "Product Not Found";
-    };
-  }
+  findCart = async () => {
+    console.log("existe", fs.existsSync(path));
+    if (fs.existsSync(path)) {
+      const data = await fs.promises.readFile(path, "utf-8");
+      const carts = JSON.parse(data);
+      return carts;
+    } else {
+      return [];
+    }
+  };
+
+  createCart = async () => {
+    const carts = await this.findCart();
+    carts.push({ id: uuidV4(), products: [] });
+    await fs.promises.writeFile(path, JSON.stringify(carts, null, "\t"));
+    return carts;
+  };
+
+  findCartById = async (id) => {
+    const carts = await this.findCart();
+
+    const cart = carts.find((cart) => {
+      return cart.id == id;
+    });
+
+    return cart ? cart : "Cart Not Found";
+  };
+
+  addProductToCart = async (idCart, idProduct) => {
+    const cart = await this.findCartById(idCart);
+
+    const index = cart.products.findIndex((product) => {
+      return product.id == idProduct;
+    });
+
+    if (index === -1) {
+      cart.products.push({ id: idProduct, quantity: 1 });
+    } else {
+      cart.products[index].quantity++;
+    }
+
+    const carts = await this.findCart();
+    const cartIndex = carts.findIndex((cartIterator) => {
+      return cartIterator.id == cart.id;
+    });
+
+    carts[cartIndex] = cart;
+
+    await fs.promises.writeFile(path, JSON.stringify(carts, null, "\t"));
+    return carts;
+  };
+}
--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -1,67 +1,31 @@
-import express from 'express';
-import fs from 'fs';
-import { v4 as uuidv4 } from 'uuid';
+import { Router } from "express";
+import CartManager from "../classes/CartManager.class.js";
 
-const router = express.Router();
+const router = Router();
+const cartManager = new CartManager();
 
-router.post('/', (req, res) => {
-  const { products } = req.body;
-  const cartId = uuidv4();
-  const newCart = {
-    id: cartId,
-    products: products || [],
-  };
-
-  const carts = JSON.parse(fs.readFileSync('carrito.json', 'utf8'));
-  carts.push(newCart);
-
-  fs.writeFileSync('carrito.json', JSON.stringify(carts, null, 2));
-
-  res.json(newCart);
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const cart = await cartManager.findCartById(id);
+  res.send(cart);
 });
 
-router.get('/:cid', (req, res) => {
-  const { cid } = req.params;
-
-  const carts = JSON.parse(fs.readFileSync('carrito.json', 'utf8'));
-  const cart = carts.find((c) => c.id === cid);
-
-  if (cart) {
-    res.json(cart.products);
-  } else {
-    res.status(404).json({ error: 'Carrito no encontrado' });
-  }
+router.get("/", async (req, res) => {
+  const carts = await cartManager.findCart();
+  res.send(carts);
 });
 
-router.post('/:cid/products/:pid', (req, res) => {
-  const { cid, pid } = req.params;
-  const { quantity } = req.body;
-
-  if (!quantity) {
-    res.status(400).json({ error: 'Falta la cantidad del producto' });
-    return;
-  }
-
-  const carts = JSON.parse(fs.readFileSync('carrito.json', 'utf8'));
-  const cartIndex = carts.findIndex((c) => c.id === cid);
-
-  if (cartIndex === -1) {
-    res.status(404).json({ error: 'Carrito no encontrado' });
-    return;
-  }
-
-  const cart = carts[cartIndex];
-  const productIndex = cart.products.findIndex((p) => p.product === pid);
-
-  if (productIndex === -1) {
-    cart.products.push({ product: pid, quantity });
-  } else {
-    cart.products[productIndex].quantity += quantity;
-  }
-
-  fs.writeFileSync('carrito.json', JSON.stringify(carts, null, 2));
-
-  res.json(cart.products);
+router.post("/", async (req, res) => {
+  await cartManager.createCart();
+  res.send({ status: "success" });
 });
+
+router.post("/:cid/products/:pid", async (req, res) => {
+  const cartId = req.params.cid
+  const productId = req.params.pid
+
+  await cartManager.addProductToCart(cartId, productId)
+  res.send({status: "succes"})
+})
 
 export default router;
